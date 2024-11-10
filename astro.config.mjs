aa8c4c18f40827e0843f6bf7e9573ce800ac6a98
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2024-11-10 12:42:03 +0100

    Remove blog-specific parts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
-
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://traductorautonomo.es",
-	integrations: [mdx(), sitemap()],
 	devToolbar: {
 		enabled: false,
 	},
