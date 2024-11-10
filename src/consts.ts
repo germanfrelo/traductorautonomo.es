b79aa99bc06d1537d0d7290328fde5f635500290
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2024-11-10 12:54:52 +0100

    Use the framework Astro (#55)
    * Migrate website to Astro
    
    * Update external links
    
    * Add the rest of files from blog template
    
    * Remove blog-specific parts
    
    * Update Datawords website link

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE =
+	"Traducteur indépendant - Tim Oetjen - Traduction EN / NL / ES > FR";
+export const SITE_DESCRIPTION =
+	"Traducteur indépendant de confiance pour gérer tous vos projets de traduction de l’anglais, du néerlandais et de l’espagnol vers le français.";
