6b63f83a2d3e181debb0f4f6a067d480925e18d3
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-02-16 19:27:40 +0100

    chore: improve Stylelint configuration (#104)
    * Allow warnings
    
    * Remove too opinionated max and min Stylelint rules
    
    * Enable HTML-like syntax parsing for only .astro files
    
    * Report disable comments that aren't scoped to rules
    
    Option available since Stylelint v16.11.0
    
    * chore(deps-dev): bump stylelint-config-standard from 36.0.1 to 37.0.0
    
    * Remove some rules already added to stylelint-config-recommended in v15.0.0
    
    Reference:
    https://github.com/stylelint/stylelint-config-recommended/commit/94cf8b60eb12ee13b8eb308579541c373e6899c6
    
    * Ignore build output
    
    * Don't lint .html files

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,14 +2,15 @@
 export default {
 	extends: [
 		// The order of configs is important: later configs take precedence over earlier ones
+		"stylelint-config-html/astro", // Enables .astro syntax parsing
 		"stylelint-config-standard", // Extends stylelint-config-recommended and turns on additional rules to enforce modern conventions
-		"stylelint-config-html", // Enables .astro syntax parsing
 		"stylelint-config-recess-order", // Sorts CSS properties
 	],
 	plugins: ["stylelint-plugin-defensive-css"], // Enforces defensive CSS best practices
 	reportDescriptionlessDisables: true,
 	reportInvalidScopeDisables: true,
 	reportNeedlessDisables: true,
+	reportUnscopedDisables: true,
 	rules: {
 		/* Avoid errors
 		---------------------------------------- */
@@ -24,8 +25,6 @@ export default {
 			{ ignoreFontFamilyNames: ["monospace"] }, // Don't report the 'font-family: monospace, monospace' declaration used in the CSS reset
 		],
 		// Unknown
-		"declaration-property-value-no-unknown": true,
-		"media-feature-name-value-no-unknown": true,
 		"no-unknown-animations": true,
 		"no-unknown-custom-media": true,
 		"no-unknown-custom-properties": true,
@@ -35,64 +34,6 @@ export default {
 		---------------------------------------- */
 		// Empty lines
 		"declaration-empty-line-before": "never",
-
-		// Max & Min
-		"max-nesting-depth": [
-			3,
-			{
-				ignore: ["blockless-at-rules", "pseudo-classes"],
-				severity: "warning",
-			},
-		],
-		"selector-max-attribute": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-class": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-compound-selectors": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-id": [
-			2,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-pseudo-class": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-specificity": [
-			"2,3,3",
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-type": [
-			3,
-			{
-				severity: "warning",
-			},
-		],
-		"selector-max-universal": [
-			2,
-			{
-				severity: "warning",
-			},
-		],
-
 		// Notation
 		"media-feature-range-notation": null, // TODO: Remove when 'baseline' is 'widely available' (~96%) (https://caniuse.com/css-media-range-syntax)
 		// Pattern
